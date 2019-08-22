d3dbdd1c07d65b48a0b31a44fb747a0d618e7082
rgscherf <rgscherf@gmail.com>
2019-08-22 15:35:11 -0700

    bump minor version #

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject snowbird "0.2.14"
+(defproject snowbird "0.2.15"
   :description "Core library for configurable static analysis as a service (CSAAAS)."
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
